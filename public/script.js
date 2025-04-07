d60bd11544cd5ea5126b328886c8cffc3dc8fcee
paulemileatn <paulemileatn@gmail.com>
2025-04-07 18:45:14 +0200

    Fix mobile menu overflow and add sticky header

--- public/script.js
+++ public/script.js
@@ -4,8 +4,8 @@ const hamburgerIcon = document.querySelector(".lucide-menu");
 const mobileMenuContainer = document.querySelector(".mobile-menu");
 const closeIcon = document.querySelector(".lucide-x");
 
-const menuHiddenClass = "-right-2/3";
-const menuVisibleClass = "right-0";
+const menuHiddenClass = "translate-x-full";
+const menuVisibleClass = "translate-x-0";
 
 const toggleMobileMenu = () => {
   mobileMenuContainer.classList.toggle(menuHiddenClass);
